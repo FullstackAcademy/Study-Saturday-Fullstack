aea3c3572e529140dacf1b0912858d9c9cbc2c2d
Noelle Laureano <nrlaureano@gmail.com>
2020-10-20 18:36:44 -0400

    Adds solution code from cycle 1

---
+++ browser/components/NewStudentForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class NewStudentForm extends Component {
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          First Name:
+          <input type='text' name='firstName' />
+        </label>
+
+        <label>
+          Last Name:
+          <input type='text' name='lastName' />
+        </label>
+
+        <label>
+          Email:
+          <input type='email' name='email' />
+        </label>
+
+        <button type='submit'>Submit New Student</button>
+      </form>
+    );
+  }
+}
