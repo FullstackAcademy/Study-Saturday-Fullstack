e99ee553b50921dab8b2497fa09a0d95336a4605
Hernan Berisso <hberisso1@gmail.com>
2021-09-30 11:35:08 -0400

    refactored Main.js and NewStudentForm.js to functional components with hooks

--- browser/components/Main.js
+++ browser/components/Main.js
@@ -1,72 +1,51 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-import StudentList from './StudentList.js';
-import SingleStudent from './SingleStudent.js';
-import NewStudentForm from './NewStudentForm';
+import StudentList from "./StudentList.js"
+import SingleStudent from "./SingleStudent.js"
+import NewStudentForm from "./NewStudentForm"
 
-export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: [],
-      selectedStudent: {},
-      displayForm: false
-    };
-  }
+const Main = (props) => {
+  const [students, setStudents] = useState([])
+  const [selectedStudent, setSelectedStudent] = useState({})
+  const [displayForm, setDisplayForm] = useState(false)
 
-  componentDidMount() {
-    this.getStudents();
-  }
+  useEffect(() => getStudents(), [])
 
-  getStudents = async () => {
+  const getStudents = async () => {
     try {
-      const { data: students } = await axios.get('/api/students');
-      this.setState({
-        students
-      });
+      const { data: students } = await axios.get("/api/students")
+      setStudents(students)
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
-
-  selectStudent = (student) => {
-    return this.setState({
-      selectedStudent: student
-    });
-  };
-
-  toggleForm = () => {
-    const currentState = this.state.displayForm;
-    this.setState({
-      displayForm: !currentState
-    });
-  };
-
-  render() {
-    const { displayForm } = this.state;
-
-    return (
-      <div>
-        <h1>Students</h1>
-        <button onClick={this.toggleForm}>Add New Student</button>
-        {displayForm ? <NewStudentForm /> : null}
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Tests</th>
-            </tr>
-          </thead>
-          <StudentList
-            students={this.state.students}
-            selectStudent={this.selectStudent}
-          />
-        </table>
-        {this.state.selectedStudent.id ? (
-          <SingleStudent student={this.state.selectedStudent} />
-        ) : null}
-      </div>
-    );
   }
+
+  const selectStudent = (student) => {
+    return setSelectedStudent(student)
+  }
+
+  const toggleForm = () => {
+    setDisplayForm(!displayForm)
+  }
+
+  return (
+    <div>
+      <h1>Students</h1>
+      <button onClick={toggleForm}>Add New Student</button>
+      {displayForm ? <NewStudentForm /> : null}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Tests</th>
+          </tr>
+        </thead>
+        <StudentList students={students} selectStudent={selectStudent} />
+      </table>
+      {selectedStudent.id ? <SingleStudent student={selectedStudent} /> : null}
+    </div>
+  )
 }
+
+export default Main
--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
+import React from "react"
 
-export default class NewStudentForm extends Component {
-  render() {
-    return (
-      <form>
-        <label>
-          First Name:
-          <input required type="text" name="firstName" />
-        </label>
+const NewStudentForm = () => {
+  return (
+    <form>
+      <label>
+        First Name:
+        <input required type="text" name="firstName" />
+      </label>
 
-        <label>
-          Last Name:
-          <input required type="text" name="lastName" />
-        </label>
+      <label>
+        Last Name:
+        <input required type="text" name="lastName" />
+      </label>
 
-        <label>
-          Email:
-          <input required type="email" name="email" />
-        </label>
+      <label>
+        Email:
+        <input required type="email" name="email" />
+      </label>
 
-        <button type="submit">Submit New Student</button>
-      </form>
-    );
-  }
+      <button type="submit">Submit New Student</button>
+    </form>
+  )
 }
+
+export default NewStudentForm
